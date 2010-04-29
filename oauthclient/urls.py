78d809953e697dc15323ba945d4657aa6bdedb9c
Alexis Metaireau <ametaireau@gmail.com>
2010-04-29 16:50:29 +0200

    add login alias to the request_token view

--- oauthclient/urls.py
+++ oauthclient/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from views import get_request_token, access_token_ready, logout
 
 urlpatterns = patterns('',
+    (r'^request_token/$', get_request_token, {}, 'login'),
     (r'^request_token/$', get_request_token, {}, 'request_token'),
     (r'^access_token_ready/$', access_token_ready, {}, 'access_token_ready'),
     (r'^logout/$', logout, {}, 'logout'),
